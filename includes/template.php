e728d94899b3574fc9351b992b6f91644482daba
Allan Lavell <alavell@gmail.com>
2013-01-24 13:43:43 -0400

    mandelbros all the way down

---
+++ includes/template.php
@@ -0,0 +1,11 @@
+//Set style sheets should take array
+
+//Set icon
+
+//Set title
+
+//Set description
+
+//Set keywords
+
+
---
+++ oldindex.php
@@ -0,0 +1,19 @@
+<html>
+<body bgcolor="black" text="white" link="blue" alink="blue" vlink="blue">
+
+<h1> MEGAMAN </h1>
+
+<p>
+<h2>Hey dudes, check out this dude playin thru megaman 1 thru 10 on July 29th, 2012 for <a href="http://www.childsplaycharity.org/">Child's Play</a>:</h2>
+
+<a href="https://www.facebook.com/events/408841719150783/424072057627749"><h1>MEGAMANATHON</h1></a>
+</p>
+
+<p><h2>
+Also, get pumped for the <a href="img/potion.gif">magical secret potion</a> I have prepared to jumpstart the <a href="img/yes.gif">awesomeness</a> that this site will contain in the near future.
+</h2></p>
+
+<img src="img/potion.gif" />
+also, check out the <a href="stream.php"><b>streamlined</b> version of this page.</a>
+</body>
+</html>
