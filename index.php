973b34bf95170034d5f45903ed2191808ebad9e2
Allan Lavell <alavell@gmail.com>
2013-02-13 12:59:41 -0400

    move thangs

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
   <body>
     <center>
       <h1 class="saying">CLICK</h1>
-      <img id="fox" src="Images/fox2.png"></img>
-      <img id="falco" src="Images/falco.png"></img>
-      <img id="dog" src="Images/dog.png"></img>
-      <img id="peppy" src="Images/peppy.png"></img>
-      <img id="slippy" src="Images/slippy.png"></img>
+      <img id="fox" src="img/fox2.png"></img>
+      <img id="falco" src="img/falco.png"></img>
+      <img id="dog" src="img/dog.png"></img>
+      <img id="peppy" src="img/peppy.png"></img>
+      <img id="slippy" src="img/slippy.png"></img>
       <h1 class="saying">CLICK</h1>
     </center>
 
     <!--<audio id="password_loop" src="Audio/password_loop.mp3" type="audio/mpeg" autoplay ></audio>-->
-    <audio id="fox_audio" src="Audio/fox.mp3" type="audio/mpeg"></audio>
-    <audio id="falco_audio" src="Audio/falco.mp3" type="audio/mpeg"></audio>
-    <audio id="dog_audio" src="Audio/dog.mp3" type="audio/mpeg"></audio>
-    <audio id="peppy_audio" src="Audio/peppy.mp3" type="audio/mpeg"></audio>
-    <audio id="slippy_audio" src="Audio/slippy.mp3" type="audio/mpeg"></audio>
+    <audio id="fox_audio" src="snd/fox.mp3" type="audio/mpeg"></audio>
+    <audio id="falco_audio" src="snd/falco.mp3" type="audio/mpeg"></audio>
+    <audio id="dog_audio" src="snd/dog.mp3" type="audio/mpeg"></audio>
+    <audio id="peppy_audio" src="snd/peppy.mp3" type="audio/mpeg"></audio>
+    <audio id="slippy_audio" src="snd/slippy.mp3" type="audio/mpeg"></audio>
   </body>
 </html>
